de7f55b250fbca65a813f0ede691f0c5073ec43a
ysfmhd369 <87653907+ysfmhd369@users.noreply.github.com>
2021-12-17 12:58:39 -0500

    Update lab4.ps1

--- powershell/lab4.ps1
+++ powershell/lab4.ps1
@@ -1,23 +1,35 @@
 ﻿#command for printing system hardware description
+function get-hardware
+{
 "Printing system hardware description"
 Get-WmiObject win32_computersystem | Format-List
 "________________________"
+}
+get-hardware
 #commands for printing operating system details
+function get-operatingsystem {
 "Printing operating system detais"
 Get-WmiObject win32_operatingsystem|Select-Object caption, version|Format-Table
-"________________________________"
+"________________________________"}
+get-operatingsystem
 #Commands for printing processor details
+function get-processor {
 "Printing processor details"
 Get-CimInstance -ClassName cim_processor | Format-List name, numberofcores,
 @{n= "L1CacheSize"; e= {switch ($_.L1CacheSize) { $null { $output = "L1 cache size does not exist"} default {$output = $_.L1CacheSize } }; $output } },
 @{n= "L2CacheSize"; e= {switch ($_.L2CacheSize) { $null { $output = "L2 cache size does not exist"} default {$output = $_.L2CacheSize } }; $output } },
 @{n= "L3CacheSize"; e= {switch ($_.L3CacheSize) { $null { $output = "L3 cache size does not exist"} default {$output = $_.L3CacheSize } }; $output } }
-"_________________________________"
+"_________________________________" }
+get-processor
 #Command for printing RAM summary
+function get-ram {
 "Printing RAM summary"
 Get-WmiObject win32_physicalmemory | Select-Object description, manufacturer, capacity, banklabel, totalphysicalmemory
 "________________________________"
+}
+get-ram
 # Commands for printing summary of physical drives
+function get-drive {
 "Printing summary of physical drive"
 $diskdrives = Get-CimInstance cim_diskdrive
 foreach ($disk in $diskdrives)
@@ -38,13 +50,18 @@ New-Object -TypeName psobject -Property @{Manufacturer=$disk.Manufacturer
 }
 }
 }
-"____________________________"
+"____________________________"}
+get-drive
 #Section for printing network adapter configuration
+function get-network {
 "Printing network adapter summary"
 Get-CimInstance win32_networkadapterconfiguration | where-object ipenabled | Select-Object description, index, ipaddress, ipsubnet, dnsdomain, dnshostname|Format-Table
-"_______________________________"
+"_______________________________"}
+get-network
 #command for printing videocard summary
+function get-video {
 "Printing video card summary"
 Get-WmiObject win32_videocontroller | Select-Object name, description, videomodedescription | Format-Table
-"___________________________________"
+"___________________________________" }
+get-video
 "_____END_____OF__________REPORT"
