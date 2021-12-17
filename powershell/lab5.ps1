00aadfc637cc8b59f94f601a6815078993247f13
ysfmhd369 <87653907+ysfmhd369@users.noreply.github.com>
2021-12-17 12:58:48 -0500

    Create lab5.ps1

---
+++ powershell/lab5.ps1
@@ -0,0 +1,15 @@
+﻿param ($System, $Disks, $Network)
+
+if ($System){
+    get-hardware
+    get-operatingsystem
+    get-processor
+    get-video
+}
+if ($Disks){
+    get-ram
+    get-drive   
+}
+if($Network){
+    get-network
+}
